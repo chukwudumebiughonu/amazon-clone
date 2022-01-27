58cca1d8a695311e35de2a58a16088719b88ffb0
chukwudumebiughonu <plasdexcel14@gmail.com>
2022-01-27 15:53:49 +0100

    Project React App

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD06-yTFs9qRI1CIkBn2ePFSVGAdz80B3g",
+    authDomain: "react-3c32e.firebaseapp.com",
+    projectId: "react-3c32e",
+    storageBucket: "react-3c32e.appspot.com",
+    messagingSenderId: "666175126905",
+    appId: "1:666175126905:web:f04d390e9d47b5504843a8",
+    measurementId: "G-CB8J6JM258"
+  };
