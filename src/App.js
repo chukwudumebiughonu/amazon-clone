5d50b1d15fd20deaa5023263de01cf5751631dc1
chukwudumebiughonu <plasdexcel14@gmail.com>
2022-01-29 11:15:41 +0100

    working on the header section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
       <h1>Hello Blonde wife let's run our precision medicine company
         👩🏼🚀 </h1>
-      {/* Header */}
+      <Header />
       {/* Home */}
     </div>
   );
