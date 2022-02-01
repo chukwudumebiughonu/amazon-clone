d8f38810f27ee99b22a3c16f754fd2610fa2f569
chukwudumebiughonu <plasdexcel14@gmail.com>
2022-02-01 15:48:12 +0100

    completing the header and  nav bar component

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Header.css";
-
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 function Header() {
   return (
     <div className="header">
@@ -12,8 +13,9 @@ function Header() {
         <input
           className='header__searchInput'
           type='text'
-        />
-        {/* { Logo } */}
+          />
+           <SearchIcon 
+          className='header__searchIcon'/> 
       </div>
       <div className='header__nav'>
         <div className='header__option'>
@@ -43,6 +45,11 @@ function Header() {
           className='header__optionLineTwo'>
             & Prime
           </span>
+          </div>
+          <div className='header__optionBasket'>
+             <ShoppingBasketIcon />
+             <span className='header__optionLineTwo 
+             header__basketCount'>0</span>
         </div>
       </div>
     </div>
