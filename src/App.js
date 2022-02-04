ec46dcb389ad3ca55e6abf383576cc7ad0e909bd
chukwudumebiughonu <plasdexcel14@gmail.com>
2022-02-04 17:33:33 +0100

    Building the checkout page with react router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import './App.css';
-import Header from './Header';
-import Home from './Home';
-
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="app">
-      <h1>Hello Blonde wife let's run our precision medicine company
-        👩🏼🚀 </h1>
-      <Header />
-      <Home/>
-    </div>
+    <Router>
+      <div className="app">
+        <h1>Hello blonde wife, lets run our precision medicine company 👩🏼🚀</h1>
+        <Switch>
+          <Route path='/checkout'>
+            <Header />
+            <h1>I am a checkout, smash the like button</h1>
+          </Route>
+          <Route path='/'>
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/Products.js
+++ src/Products.js
@@ -1,26 +1,28 @@
-import React from "react";
-import "./Product.css";
+import React from 'react';
+import './Product.css';
 
-function Products({id, title, image, price, rating }) {
-  return (
-    <div className="product">
-      <div className="product__info">
-        <p>{title}</p>
-        <p className="product__price">
-          <small>$</small>
-          <strong>{price}</strong>
-        </p>
-        <div className="product__rating">
+
+function Products({id,title, image, price, rating }) {
+  return( <div 
+  className='product'>
+    <div className='product__info'>
+       <p>{title}</p>
+       <p className='product__price'>
+           <small>$</small>
+           <strong>{price}</strong>
+       </p>
+       <div className='product__rating'>
         {Array(rating)
         .fill()
-        .map((_, i) => (
-            <p>🌟</p>
-          ))}
-        </div>
-      </div>
-      <img src={image} alt=""/>
-      <button>Add to basket</button>
-    </div>
+        .map((_, i) =>(
+        <p> 🌟 </p>
+        ))}
+        
+       </div>
+    </div> 
+           <img src={image } alt=''/>
+           <button>Add to Basket</button>
+  </div>
   );
 }
 
